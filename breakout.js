8db8d6e46eeb151e18c686f255c842d54cde0fc0
Osama Suleiman <132921363+HappyDay101@users.noreply.github.com>
2023-12-14 07:42:24 +0000

    made the outofbound functionality for the player

--- breakout.js
+++ breakout.js
@@ -41,11 +41,23 @@ function update() {
     context.fillRect(player.x, player.y, player.width, player.height);
 }
 
+function outOfBounds(xPosition) {
+    return (xPosition < 0 || xPosition + playerWidth > boardWidth);
+}
+
 function movePlayer(event) {
     if (event.code == "ArrowLeft") {
-        player.x -= player.velocityX;
+        // player.x -= player.velocityX;
+        let nextPlayerX =player.x - player.velocityX;
+        if (!outOfBounds(nextPlayerX)) {
+            player.x = nextPlayerX;
+        }
     }
     else if (event.code == "ArrowRight") {
         player.x += player.velocityX
+        let nextPlayerX = player.x + player.velocityX;
+        if (!outOfBounds(nextPlayerX)) {
+            player.x = nextPlayerX;
+        }
     }
 }
