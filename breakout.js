fd8a950e85a27d813e8b3251ad67c25e24134e03
Osama Suleiman <132921363+HappyDay101@users.noreply.github.com>
2023-12-15 06:06:17 +0000

    made the ball

--- breakout.js
+++ breakout.js
@@ -17,6 +17,21 @@ let player = {
     velocityX : playerVelocityX
 }
 
+// ball
+let ballWidth = 10;
+let ballHeight = 10;
+let ballVelocityX = 3;
+let ballVelocityY = 2;
+
+let ball = {
+    x : ballWidth/2,
+    y : ballHeight/2,
+    width : ballWidth,
+    height : ballHeight,
+    velocityX : ballVelocityX,
+    velocityY : ballVelocityY
+}
+
 windwow.onload = function() {
     board = document.getElementById("board");
     board.height = boardHeight;
@@ -39,6 +54,11 @@ function update() {
     // player
     context.fillStyle = "lightgreen";
     context.fillRect(player.x, player.y, player.width, player.height);
+
+    context.fillStyle = "white";
+    ball.x += ball.velocityX;
+    ball.y + ball.ballVelocityY;
+    context.fillRect(ball.x, ball.y, ball.width, ball.height); 
 }
 
 function outOfBounds(xPosition) {
@@ -54,7 +74,7 @@ function movePlayer(event) {
         }
     }
     else if (event.code == "ArrowRight") {
-        player.x += player.velocityX
+        // player.x += player.velocityX
         let nextPlayerX = player.x + player.velocityX;
         if (!outOfBounds(nextPlayerX)) {
             player.x = nextPlayerX;
